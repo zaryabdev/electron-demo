11a4f35863b6a036c98c3c14a7ca7fa3bf08375a
Zaryab <zaryabdev404@gmail.com>
2022-07-23 01:29:36 +0500

    added electron

---
+++ renderer.js
@@ -0,0 +1,15 @@
+const information = document.getElementById('info');
+information.innerText = `
+    This app is using
+    Chrome (v${versions.chrome()}),
+    Node.js (v${versions.node()}),
+    Electron (v${versions.electron()})
+`;
+
+const func = async () => {
+    const response = await window.versions.ping();
+    console.log(response);
+    // prints out 'pong'
+};
+
+func();
